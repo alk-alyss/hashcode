3910e3fef93b4805b59d95656ea0d41b185db6f1
Alkinoos Alyssandrakis <alk.alyssandrakis@gmail.com>
2022-02-24 20:41:56 +0200

    parse data

--- problemNoe.py
+++ problemNoe.py
@@ -5,8 +5,44 @@ class Project:
 		self.score = score
 		self.end = end
 		self.roles = roles
+	def __str__(self):
+		return str(self.name)+" "+str(self.duration)+" "+str(self.score)+" "+str(self.end)+" "+str(self.roles)
 
 class Contributor:
 	def __init__(self, name, skills):
 		self.name = name
 		self.skills = skills # dictionary: key=skill, value=level
+		self.working = False
+	def __str__(self):
+		return str(self.name)+" "+str(self.skills)
+
+def readInput(filename):
+	contributors = []
+	projects = []
+	with open(filename, "r") as f:
+		c, p = f.readline().split()
+		for x in range(int(c)):
+			name, n = f.readline().split()
+			skills = {}
+			for y in range(int(n)):
+				skill, level = f.readline().split()
+				skills.update({skill : level})
+			
+			contributors.append(Contributor(name, skills))
+
+		for x in range(int(p)):
+			name, d, s, e, r = f.readline().split()
+			roles = {}
+			for y in range(int(r)):
+				role, level = f.readline().split()
+				roles.update({role : level})
+			
+			projects.append(Project(name, d, s, e, roles))
+	
+	return contributors, projects
+
+contributors, projects = readInput("a_an_example.in.txt")
+for c in contributors:
+	print(c)
+for p in projects:
+	print(p)
