834f374a5915bef26560fd520dc99d23048fa214
Alkinoos Alyssandrakis <alk.alyssandrakis@gmail.com>
2022-02-24 20:07:45 +0200

    init

---
+++ problem.py
@@ -0,0 +1,15 @@
+class Project:
+	def __init__(self, name, duration, score, end, roles):
+		self.name = name
+		self.duration = duration
+		self.score = score
+		self.end = end
+		self.roles = roles
+
+class Contributor:
+	def __init__(self, name, skills):
+		self.name = name
+		self.skills = skills
+
+
+	
