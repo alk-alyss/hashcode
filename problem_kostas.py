feb633b19379ec652014c987d182a2de809304c5
KonstantoJr <konstantokon29@gmail.com>
2022-02-24 20:47:35 +0200

    Add files via upload

--- problem_kostas.py
+++ problem_kostas.py
@@ -1,13 +1,11 @@
-def read_file(filename):
-	
-
 class Project:
-    def __init__(self, name: str, duration: int, score: int, end: int, roles: list):
+    def __init__(self, name: str, duration: int, score: int, end: int, roles: dict):
         self.name = name
         self.duration = duration
         self.score = score
         self.end = end
         self.roles = roles
+        self.contributors = []
 
 
 class Contributor:
@@ -15,5 +13,17 @@ class Contributor:
         self.name = name
         self.skills = skills
 
+    def check_project(self, project: Project) -> bool:
+        for i in project.roles.keys():
+            if i in self.skills.keys():
+                if self.skills[i] >= project.roles[i]:
+                    return i
+        return False
 
 
+def main():
+    return
+
+
+if __name__ == "__main__":
+    main()
