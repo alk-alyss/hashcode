331770c98fe2dce8c3996df86d4b12fe4ec40f47
Alkinoos Alyssandrakis <alk.alyssandrakis@gmail.com>
2022-02-24 22:26:32 +0200

    changes

--- problemNoe.py
+++ problemNoe.py
@@ -1,121 +1,123 @@
 class Project:
-    def __init__(self, name, duration, score, end, roles):
-        self.name = name
-        self.duration = int(duration)
-        self.score = int(score)
-        self.end = int(end)
-        self.roles = roles
-        self.contributors = []
+	def __init__(self, name, duration, score, end, roles):
+		self.name = name
+		self.duration = int(duration)
+		self.score = int(score)
+		self.end = int(end)
+		self.roles = roles
+		self.contributors = []
 
-    def getContributors(self, contributors):
-        for role in self.roles:
-            for contributor in contributors:
-                if not contributor.working:
-                    if contributor.get_skill(role[0]) >= role[1]:
-                        self.contributors.append(contributor)
-                        contributor.working = True
-                        break
-            else:
-                return False
-        return True
+	def getContributors(self, contributors):
+		for role in self.roles:
+			for contributor in contributors:
+				if not contributor.working:
+					if contributor.get_skill(role[0]) >= role[1]:
+						if contributor.get_skill(role[0]) == role[1]:
+							contributor.impoveSkill(role[0])
+						self.contributors.append(contributor)
+						contributor.working = True
+						break
+			else:
+				return False
+		return True
 
-    def __str__(self):
-        # return str(self.name)+" "+str(self.duration)+" "+str(self.score)+" "+str(self.end)+" "+str(self.roles)
-        return "Score: " + str(self.score) + " End: " + str(self.end)
+	def __str__(self):
+		# return str(self.name)+" "+str(self.duration)+" "+str(self.score)+" "+str(self.end)+" "+str(self.roles)
+		return "Score: " + str(self.score) + " End: " + str(self.end)
 
 
 class Contributor:
-    def __init__(self, name, skills):
-        self.name = name
-        self.skills = skills  # dictionary: key=skill, value=level
-        self.working = False
+	def __init__(self, name, skills):
+		self.name = name
+		self.skills = skills  # dictionary: key=skill, value=level
+		self.working = False
 
-    def get_skill(self, skill) -> bool:
-        if skill in self.skills.keys():
-            return self.skills[skill]
-        return 0
+	def get_skill(self, skill) -> bool:
+		if skill in self.skills.keys():
+			return self.skills[skill]
+		return 0
 
-    def __str__(self):
-        return str(self.name) + " " + str(self.skills)
+	def __str__(self):
+		return str(self.name) + " " + str(self.skills)
 
-    def check_project(self, project: Project) -> bool:
-        for i in project.roles.keys():
-            if i in self.skills.keys():
-                if self.skills[i] >= project.roles[i]:
-                    return i
-        return False
+	def check_project(self, project: Project) -> bool:
+		for i in project.roles.keys():
+			if i in self.skills.keys():
+				if self.skills[i] >= project.roles[i]:
+					return i
+		return False
 
-    def improveSkill(self, skill):
-        if skill in self.skills.keys():
-            self.skills[skill] += 1
-        else:
-            self.skills[skill] = 1
+	def improveSkill(self, skill):
+		if skill in self.skills.keys():
+			self.skills[skill] += 1
+		else:
+			self.skills[skill] = 1
 
 
 def readInput(filename):
-    contributors = []
-    projects = []
-    with open(filename, "r") as f:
-        c, p = f.readline().split()
-        for x in range(int(c)):
-            name, n = f.readline().split()
-            skills = {}
-            for y in range(int(n)):
-                skill, level = f.readline().split()
-                skills[skill] = int(level)
+	contributors = []
+	projects = []
+	with open(filename, "r") as f:
+		c, p = f.readline().split()
+		for x in range(int(c)):
+			name, n = f.readline().split()
+			skills = {}
+			for y in range(int(n)):
+				skill, level = f.readline().split()
+				skills[skill] = int(level)
 
-            contributors.append(Contributor(name, skills))
+			contributors.append(Contributor(name, skills))
 
-        for x in range(int(p)):
-            name, d, s, e, r = f.readline().split()
-            roles = []
-            for y in range(int(r)):
-                role, level = f.readline().split()
-                roles.append((role, int(level)))
+		for x in range(int(p)):
+			name, d, s, e, r = f.readline().split()
+			roles = []
+			for y in range(int(r)):
+				role, level = f.readline().split()
+				roles.append((role, int(level)))
 
-            projects.append(Project(name, d, s, e, roles))
+			projects.append(Project(name, d, s, e, roles))
 
-    return contributors, projects
+	return contributors, projects
 
 
 def sortProjects(projects):
-    newProjects = sorted(projects, key=lambda x: (x.score, x.end), reverse=True)
-    return newProjects
+	newProjects = sorted(projects, key=lambda x: (x.score, x.end), reverse=True)
+	return newProjects
 
 
 def assignContributors(projects):
-    working = []
-    while True:
-        project = projects.pop()
-        if not project.getContributors(contributors):
-            break
-        working.append(project)
+	working = []
+	for project in projects:
+		if not project.getContributors(contributors):
+			continue
+		working.append(project)
+		projects.remove(project)
 
-    return working
+	return working
 
 
 def completeProject(working):
-    global day, score
+	global day, score
 
-    working = sorted(working, key=lambda x: x.duration - day)
-    currentProject = working.pop()
-    day += currentProject.duration - day
-    score += max(currentProject.score - (currentProject.end - currentProject.day), 0)
-    for c in currentProject.contributors:
-        c.working = False
+	working = sorted(working, key=lambda x: x.duration - day)
+	currentProject = working.pop()
+	day += currentProject.duration - day
+	score += max(currentProject.score - (currentProject.end - day), 0)
+	for c in currentProject.contributors:
+		c.working = False
 
-    return currentProject
+	return currentProject
 
 
 def write_submission(done, filename):
-    with open(filename, "w") as file:
-        file.write(str(len(done)))
-        for project in done:
-            file.write(project.name)
-            tmp = ""
-            for contributor in project.contributors:
-                tmp += contributor.name + " "
-            file.write(tmp)
+	with open(filename, "w") as file:
+		file.write(str(len(done)))
+		for project in done:
+			file.write("\n" + project.name)
+			tmp = "\n"
+			for contributor in project.contributors:
+				tmp += contributor.name + " "
+			file.write(tmp)
 
 
 # Main code
@@ -130,11 +132,11 @@ projects = sortProjects(projects)
 done = []
 
 while True:
-    working = assignContributors(projects)
-    if working == []:
-        break
-    done.append(completeProject(working))
-    if len(done) == numberOfProjects:
-        break
+	working = assignContributors(projects)
+	if working == []:
+		break
+	done.append(completeProject(working))
+	if len(done) == numberOfProjects:
+		break
 
 write_submission(done, "a_submission.txt")
