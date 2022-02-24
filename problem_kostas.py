03584c7d1a7944c74312d10d05098b46ec5218fd
KonstantoJr <konstantokon29@gmail.com>
2022-02-24 20:17:06 +0200

    Add files via upload

---
+++ problem_kostas.py
@@ -0,0 +1,19 @@
+def read_file(filename):
+	
+
+class Project:
+    def __init__(self, name: str, duration: int, score: int, end: int, roles: list):
+        self.name = name
+        self.duration = duration
+        self.score = score
+        self.end = end
+        self.roles = roles
+
+
+class Contributor:
+    def __init__(self, name: str, skills: dict):
+        self.name = name
+        self.skills = skills
+
+
+
